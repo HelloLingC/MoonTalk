5d13eca804cbd4daa8a156a010ba4c3fa200a3e5
hellolingc <hellolingc@gmail.com>
2025-04-25 22:37:11 +0800

    vercel.json

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', 'https://moonlab.top');
+    res.header('Access-Control-Allow-Origin', 'https://moonlab.top');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     // Allow specific headers
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
